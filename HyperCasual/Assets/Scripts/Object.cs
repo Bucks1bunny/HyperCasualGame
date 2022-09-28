c4497a213cf4c11a1f44023c9a89b533e7da06b9
Rodion Peichev <46489335+Bucks1bunny@users.noreply.github.com>
2022-09-28 23:32:23 +0200

    Added Dotween and smooth food animation

--- HyperCasual/Assets/Scripts/Object.cs
+++ HyperCasual/Assets/Scripts/Object.cs
@@ -1,37 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Object : MonoBehaviour, IUpdateable
 {
     [SerializeField]
     private string _tag;
     private Rigidbody rb;
-    private Vector3 destination = new Vector3(-6.87f, 1.413f, -1.592f);
+    private Vector3 startPosition;
+    private Vector3 destination = new Vector3(-6.87f, 1.413f, -1.62f);
+
     public void Tick()
     {
-        transform.Translate(destination * 1 * Time.deltaTime);
         if (transform.position == destination)
         {
+            rb.freezeRotation = false;
             rb.useGravity = true;
+            ObjectPool.Instance.SpawnFromPool(_tag, startPosition, Quaternion.identity);
+            UpdateManager.UnregisterLogic(this);
         }
     }
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
         rb.useGravity = false;
     }
 
     private void OnMouseDown()
     {
-        Debug.Log("Click");
+        startPosition = transform.position;
 
-        ObjectPool.Instance.SpawnFromPool(_tag, transform.position, Quaternion.identity);
+        transform.DOMoveY(1.3f, 1f).OnComplete(() => transform.DOMove(destination, 2f).SetEase(Ease.OutSine));
+        transform.DORotate(new Vector3(90, 0, 0), 3f);
+        transform.DOScale(transform.localScale / 1.75f, 2.5f);
 
-        transform.localScale = transform.localScale / 1.75f;
-        transform.rotation = Quaternion.Euler(90, 0, 0);
-        transform.position = destination;
-        rb.useGravity = true;
+        UpdateManager.RegisterLogic(this);
     }
 }
